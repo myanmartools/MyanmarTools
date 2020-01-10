dc5b9d6419f7fd56a98f0e5ae981a3c5f65396ad
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-10 13:10:56 +0630

    Add myanmar-text-fragmenter-js

---
+++ src/app/myanmar-text-fragmenter-js/myanmar-text-fragmenter-js.component.ts
@@ -0,0 +1,19 @@
+
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { Component } from '@angular/core';
+
+/**
+ * The Myanmar Text Fragmenter Js page component.
+ */
+@Component({
+    selector: 'app-myanmar-text-fragmenter-js',
+    templateUrl: './myanmar-text-fragmenter-js.component.html'
+})
+export class MyanmarTextFragmenterJsComponent { }
