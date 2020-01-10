6adc0c723c1b99a91e637501bc94dbead87a3c00
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-10 13:10:32 +0630

    Add myanmar-words

---
+++ src/app/myanmar-words/myanmar-words.component.ts
@@ -0,0 +1,19 @@
+
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { Component } from '@angular/core';
+
+/**
+ * The Myanmar Words page component.
+ */
+@Component({
+    selector: 'app-myanmar-words',
+    templateUrl: './myanmar-words.component.html'
+})
+export class MyanmarWordsComponent { }
